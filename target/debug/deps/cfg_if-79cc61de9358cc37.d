3c7653a501b315f1aac3bc05dd5c6edabd1e5a49
cepalle <epalle.cyrille@gmail.com>
2021-04-17 19:19:30 +0200

    rm trash file

--- target/debug/deps/cfg_if-79cc61de9358cc37.d
+++
@@ -1,7 +0,0 @@
-C:\Users\cyrille\IDEA\bomberman\target\debug\deps\cfg_if-79cc61de9358cc37.rmeta: C:\Users\cyrille\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\cyrille\IDEA\bomberman\target\debug\deps\libcfg_if-79cc61de9358cc37.rlib: C:\Users\cyrille\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\cyrille\IDEA\bomberman\target\debug\deps\cfg_if-79cc61de9358cc37.d: C:\Users\cyrille\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\cyrille\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs:
