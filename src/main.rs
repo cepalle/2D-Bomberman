f7b6c89e8e4b4534637141221363f730f9a8b00c
cepalle <epalle.cyrille@gmail.com>
2021-05-03 21:08:20 +0200

    2D starter

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,45 @@
-use bevy::prelude::*;
+use amethyst::{
+    core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    ui::{RenderUi, UiBundle},
+    utils::application_root_dir,
+};
 
-struct Person;
-struct Name(String);
+mod state;
 
-fn hello_world() {
-    println!("hello world!");
-}
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
 
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
-    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
-    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
-}
+    let app_root = application_root_dir()?;
 
-fn greet_people(
-    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
+    let resources = app_root.join("assets");
+    let display_config = app_root.join("config/display_config.ron");
+    let key_bindings_path = app_root.join("config/input.ron");
 
-struct GreetTimer(Timer);
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            InputBundle::<StringBindings>::new().with_bindings_from_file(&key_bindings_path)?,
+        )?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config)?
+                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                )
+                .with_plugin(RenderUi::default())
+                .with_plugin(RenderFlat2D::default()),
+        )?;
 
-pub struct HelloPlugin;
+    let mut game = Application::new(resources, state::MyState, game_data)?;
+    game.run();
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        // the reason we call from_seconds with the true flag is to make the timer repeat itself
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
-    }
-}
-
-fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
-        .run();
+    Ok(())
 }
